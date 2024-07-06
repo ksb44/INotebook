b25c30815e58089e53cda174fdfd39ecaec67d9b
Kuldeep Singh Bartwal <71578263+ksb44@users.noreply.github.com>
2024-07-06 14:47:15 +0530

    Update Login.js

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -6,7 +6,7 @@ function Login() {
   };
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch("http://localhost:8000/api/auth/login", {
+    const response = await fetch("https://vercel.com/ksb44s-projects/i-notebook-tmak/api/auth/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
